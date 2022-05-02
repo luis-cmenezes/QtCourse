8db79f3e9104a881db1e737e906ff6d889e8823d
Luis Costa <lipemenezescosta@gmail.com>
2022-05-02 15:03:31 -0300

    Estudo da transiçao entre objetos nao renderizados para objetos de malha na visualizaçao.

--- PyQt/PyQtGraph/pygraph.py
+++ PyQt/PyQtGraph/pygraph.py
@@ -1,9 +1,14 @@
-from PyQt5 import QtWidgets, uic
-from pyqtgraph import PlotWidget
+from PyQt5 import QtWidgets
 from ui_config import Ui_MainWindow
 
 import pyqtgraph as pg
+import pyqtgraph.opengl as gl
+import numpy as np
 import sys
+
+SUPERIOR_LIMIT = 430
+INFERIOR_LIMIT = 320
+COLORS_DICT = {'above': (1.0,0.0,0.0,1.0), 'below': (0.0,0.0,1.0,0.0), 'normal': (0.0,1.0,0.0,1.0)}
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -12,10 +17,87 @@ class MainWindow(QtWidgets.QMainWindow):
         
         self.ui  = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.plot([1,2,3,4,5,6,7,8,9,10], [30,32,34,32,33,31,29,32,35,45])
 
-    def plot(self, hour, temperature):
-        self.ui.graphWidget.plot(hour, temperature)
+        self.ui.graphicsView.setCameraPosition(distance=300)
+        self.importFiles()
+        self.createScatterObject()
+        self.createMeshObject()
+    
+        self.ui.change_marker_btn.clicked.connect(lambda: self.changeMarker())
+        self.isItemVisibile = False
+    
+    def importFiles(self):
+        self.trajectory = []
+        with open('PyQtGraph/reduced_trajectory.txt', 'r') as file:
+            traj_lines = file.readlines()
+
+        for line in traj_lines:
+            coords = (line.split(' '))
+            coords = tuple(float(coord) for coord in coords)
+
+            self.trajectory.append(coords)
+
+
+        acquisition = []
+        with open('PyQtGraph/retI-retV-rmsI-rmsV-heatInput-T.txt', 'r') as file:
+            acq_lines = file.readlines()
+
+        for line in acq_lines:
+            data = (line.split(' '))
+            data = [float(n_data) for n_data in data]
+
+            acquisition.append(data)
+
+        self.generateColorList(acquisition)
+
+    def generateColorList(self, acquisition):
+        self.color = np.empty((len(acquisition), 4))
+        for index, row in enumerate(acquisition):
+
+            current_data = row[4]
+            if current_data >= INFERIOR_LIMIT and current_data <= SUPERIOR_LIMIT:
+                self.color[index] = (0.0, 1.0, 0.0, 1.0)
+            elif current_data > SUPERIOR_LIMIT:
+                self.color[index] = (1.0, 0.0, 0.0, 1.0)   
+            elif current_data < INFERIOR_LIMIT:
+                self.color[index] = (0.0, 0.0, 1.0, 1.0)
+
+    def createScatterObject(self):
+        self.scatter_object = gl.GLScatterPlotItem(pos=self.trajectory, size=1, color=self.color, pxMode=False)
+    
+    def createMeshObject(self):
+        self.mesh_objects_list = []
+
+        sphere_mesh = gl.MeshData.sphere(rows=40, cols=40)
+        for index, point in enumerate(self.trajectory):
+            sphere = gl.GLMeshItem(meshdata=sphere_mesh, smooth=True, color=self.color[index], shader='shaded', glOptions='opaque')
+            sphere.translate(point[0], point[1], point[2])
+
+            self.mesh_objects_list.append(sphere)
+        
+    def setMeshVisibility(self, action):
+        if action:
+            for sphere in self.mesh_objects_list:
+                self.ui.graphicsView.addItem(sphere)
+        else:
+            for sphere in self.mesh_objects_list:
+                self.ui.graphicsView.removeItem(sphere)
+
+    def changeMarker(self):
+        if self.isItemVisibile:
+            try:
+                self.isItemVisibile = False
+                self.ui.graphicsView.removeItem(self.scatter_object)
+                self.setMeshVisibility(True)
+            except:
+                pass
+        else:
+            try:
+                self.isItemVisibile = True
+                self.ui.graphicsView.addItem(self.scatter_object)
+                self.setMeshVisibility(False)
+            except:
+                pass
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
--- PyQt/PyQtGraph/ui_config.py
+++ PyQt/PyQtGraph/ui_config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'graph_test.ui'
+## Form generated from reading UI file 'pyqtgraph_test.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -12,24 +12,44 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
-from pyqtgraph import PlotWidget
+from pyqtgraph.opengl import GLViewWidget
 
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(842, 560)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.horizontalLayout = QHBoxLayout(self.centralwidget)
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.graphicsView = GLViewWidget(self.centralwidget)
+        self.graphicsView.setObjectName(u"graphicsView")
+
+        self.verticalLayout.addWidget(self.graphicsView)
+
+        self.frame = QFrame(self.centralwidget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setMinimumSize(QSize(0, 30))
+        self.frame.setMaximumSize(QSize(16777215, 30))
+        self.frame.setFrameShape(QFrame.NoFrame)
+        self.frame.setFrameShadow(QFrame.Plain)
+        self.horizontalLayout = QHBoxLayout(self.frame)
         self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.graphWidget = PlotWidget(self.centralwidget)
-        self.graphWidget.setObjectName(u"graphWidget")
+        self.change_marker_btn = QPushButton(self.frame)
+        self.change_marker_btn.setObjectName(u"change_marker_btn")
+        self.change_marker_btn.setMinimumSize(QSize(200, 0))
+        self.change_marker_btn.setMaximumSize(QSize(200, 16777215))
 
-        self.horizontalLayout.addWidget(self.graphWidget)
+        self.horizontalLayout.addWidget(self.change_marker_btn)
+
+
+        self.verticalLayout.addWidget(self.frame)
 
         MainWindow.setCentralWidget(self.centralwidget)
 
@@ -40,5 +60,6 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.change_marker_btn.setText(QCoreApplication.translate("MainWindow", u"Renderizar esferas", None))
     # retranslateUi
 
